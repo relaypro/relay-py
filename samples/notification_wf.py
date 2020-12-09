e0ba17f38d82e36cee95edcb25d28209442db5f5
Steve Ims <sims@republicwireless.com>
2020-12-09 15:19:12 -0500

    fix notify wf

--- samples/notification_wf.py
+++ samples/notification_wf.py
@@ -8,17 +8,17 @@ wf = relay.workflow.Workflow(__name__)
 @wf.on_start
 async def start_handler(relay):
     text = await relay.get_var('text')
-    target = await relay.get_var('targets')
+    targets = (await relay.get_var('targets')).split(',')
     ntype = await relay.get_var('type')
 
     if ntype == 'broadcast':
-        await relay.broadcast(text, target)
+        await relay.broadcast(text, targets)
 
     elif ntype == 'notify':
-        await relay.notify(text, target)
+        await relay.notify(text, targets)
 
     elif ntype == 'alert':
-        await relay.notify(text, target)
+        await relay.notify(text, targets)
 
 @wf.on_notification(event='ack_event')
 async def ack_handler(relay, source, event):
