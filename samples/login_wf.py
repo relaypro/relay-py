a61d8b9449455fc239ae56ed89902bfacc7450d4
Steve Ims <sims@republicwireless.com>
2020-12-09 14:09:23 -0500

    fix login wf

--- samples/login_wf.py
+++ samples/login_wf.py
@@ -8,7 +8,7 @@ wf = relay.workflow.Workflow(__name__)
 @wf.on_start
 async def start_handler(relay):
     label = await relay.get_var('match_spillover', None)
-    if name:
+    if label:
         await relay.say(f'setting the name for this device to {label}')
         await relay.set_device_name(label)
 
