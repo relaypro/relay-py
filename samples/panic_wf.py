edbc4f002cbe4ba91152d422d6669d66c94efe5f
Steve Ims <sims@republicwireless.com>
2020-12-10 14:10:01 -0500

    fix panic wf

--- samples/panic_wf.py
+++ samples/panic_wf.py
@@ -8,7 +8,7 @@ wf = relay.workflow.Workflow(__name__)
 @wf.on_start
 async def start_handler(relay):
     itype = await relay.get_var('incident_type', 'demo')
-    await relay.create_incident('itype')
+    await relay.create_incident(itype)
     await relay.say('created an incident')
     #await relay.resolve_incident()
     await relay.terminate()
