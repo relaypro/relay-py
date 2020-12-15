ee691582db27a623eed78ac4d7e94462c71dac6c
Steve Ims <sims@republicwireless.com>
2020-12-15 10:11:01 -0500

    handle exceptions related to location

--- samples/deviceinfo_demo_wf.py
+++ samples/deviceinfo_demo_wf.py
@@ -9,9 +9,20 @@ wf = relay.workflow.Workflow(__name__)
 async def start_handler(relay):
     label = await relay.get_device_label()
     await relay.say(f'This device is {label}')
-    address = await relay.get_device_address()
-    await relay.say(f'The device is located at the following street address {address}')
-    indoor_location = await relay.get_device_indoor_location()
-    await relay.say(f"The device's indoor location is {indoor_location}")
+
+    try:
+        address = await relay.get_device_address()
+        await relay.say(f'The device is located at the following street address {address}')
+
+    except WorkflowException:
+        await relay.say('failed to get address; is location enabled?')
+
+    try:
+        indoor_location = await relay.get_device_indoor_location()
+        await relay.say(f"The device's indoor location is {indoor_location}")
+
+    except WorkflowException:
+        await relay.say('failed to get indoor location')
+
     await relay.terminate()
 
