156d619f4d060279aa40860c7028a321ff436bb2
shakeebshams <shakeebshams@gmail.com>
2021-06-25 13:28:35 -0400

    add core for translate

--- relay/workflow.py
+++ relay/workflow.py
@@ -473,4 +473,13 @@ class Relay:
             }
         await self.sendReceive(event)
 
+    async def translate(self, text, from='en-US', to='es-ES'):
+        event = {
+            '_type': 'wf_api_translate_request',
+            'text': text,
+            'from_lang': from,
+            'to_lang': to
+        }
+        translatedText = await self.sendReceive(event)
+        return translatedText
 
