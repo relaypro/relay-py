cd7827160b257ae5a3352c666ae872f84095e1a1
Ashley Godwin <agodwin@relaypro.com>
2022-06-16 17:11:03 -0400

    Updated Hello World workflow

--- samples/hello_world_wf.py
+++ samples/hello_world_wf.py
@@ -22,49 +22,19 @@ async def start_handler(relay, trigger):
 
     device_urn = trigger['args']['source_uri']
     logger.debug(f'device_urn is {device_urn}')
-    
-    groupId = await uri.group_id('Main long')
-    print(f'GROUP ID: {groupId}')
-    groupName = await uri.group_name('Main')
-    print(f'GROUP NAME: {groupName}')
-    groupNameLong = await uri.group_name('Main Long Name')
-    print(f'GROUP NAME LONG: {groupNameLong}')
-    deviceName = await uri.device_name('Rabbit')
-    print(f'DEVICE NAME: {deviceName}')
-    deviceNameLong = await uri.device_name('Rabbit Hole')
-    print(f'DEVICE NAME LONG: {deviceNameLong}')
-    groupMember = await uri.group_member('Main Long', 'Froggy Frog')
-    print(f'GROUP MEMBER: {groupMember}')
-    deviceId = await uri.device_id('990003883833883')
-    print(f'DEVICE ID: {deviceId}')
-    parseGroupName = await uri.parse_group_name('urn:relay-resource:name:group:security%20one')
-    print(f'PARSE GROUP NAME: {parseGroupName}')
-    parseGroupId = await uri.parse_group_id('urn:relay-resource:id:group:security')
-    print(f'PARSE GROUP ID: {parseGroupId}')
-    parseDeviceName = await uri.parse_device_name('urn:relay-resource:name:device:jim')
-    print(f'PARSE DEVICE NAME: {parseDeviceName}')
-    parseDeviceNameInteraction = await uri.parse_device_name('urn:relay-resource:name:interaction:hello%20world?device=urn%3Arelay-resource%3Aname%3Adevice%3ACam')
-    print(f'PARSE DEVICE NAME INTERACTION: {parseDeviceNameInteraction}')
-    parseDeviceId = await uri.parse_device_id('urn:relay-resource:id:device:990000838383838')
-    print(f'PARSE DEVICE ID: {parseDeviceId}')
-    isInteractionUri = await uri.is_interaction_uri('urn:relay-resource:name:interaction:hello%20world?device=urn%3Arelay-resource%3Aname%3Adevice%3ACam')
-    print(f'IS INTERACTION URI: {isInteractionUri}')
-    isRelayUri = await uri.is_relay_uri('urn:relay-resource:id:device:990000838383838')
-    print(f'IS RELAY URI: {isRelayUri}')
-    isNotInteractionUri = await uri.is_interaction_uri('urn:relay-resource:id:device:990000838383838')
-    print(f'IS NOT INTERACTION URI: {isNotInteractionUri}')
-    isNotRelayUri = await uri.is_relay_uri('mickey:mouse')
-    print(f'IS NOT RELAY URI: {isNotRelayUri}')
-    await relay.terminate()
-    # await relay.terminate()
-#     await relay.start_interaction(target, 'my interaction')
+    target = relay.target_from_trigger(trigger)
+
+    await relay.start_interaction(target, 'hello world')
 
 
-# @wf.on_interaction_lifecycle
-# async def lifecycle_handler(relay, itype, source_uri, reason):
-#     if itype == 'started':
-#         logger.debug('interaction started in app')
-#         target = relay.targets_from_source_uri(source_uri)
-        
-#         await relay.end_interaction(target, 'myinteraction')
-#         await relay.terminate()
+@wf.on_interaction_lifecycle
+async def lifecycle_handler(relay, itype, source_uri, reason):
+    if itype == 'started':
+        logger.debug('interaction started in app')
+        target = relay.targets_from_source_uri(source_uri)
+        name = await relay.get_device_name(target)
+        await relay.say_and_wait(target, 'What is your name?')
+        user = await relay.listen(target, 'request1')
+        await relay.say_and_wait(target, f'Hello {user}! from {name}')
+        await relay.end_interaction(target, 'myinteraction')
+        await relay.terminate()
