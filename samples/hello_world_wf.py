5ce9fdceaf404d2492e3147f0c4a315e8ca57517
Ashley Godwin <agodwin@relaypro.com>
2022-06-27 16:54:19 -0400

    Updated Hello World sample

--- samples/hello_world_wf.py
+++ samples/hello_world_wf.py
@@ -6,7 +6,6 @@ import asyncio
 import os
 import relay.workflow
 import logging
-
 import time
 
 logger = logging.getLogger(__name__)
@@ -14,16 +13,13 @@ logger.setLevel(logging.DEBUG)
 
 wf = relay.workflow.Workflow('hello')
 port = os.getenv('PORT')
-uri = relay.workflow.Uri
 if port is None:
     port = 8080
 
 @wf.on_start
 async def start_handler(relay, trigger):
-    logger.debug(f"mywf trigger={trigger}")
 
     device_urn = trigger['args']['source_uri']
-    logger.debug(f'device_urn is {device_urn}')
     target = relay.make_target_uris(trigger)
 
     await relay.start_interaction(target, 'hello world')
@@ -32,11 +28,12 @@ async def start_handler(relay, trigger):
 @wf.on_interaction_lifecycle
 async def lifecycle_handler(relay, itype, source_uri, reason):
     if itype == 'started':
-        logger.debug('interaction started in app')
         target = relay.targets_from_source_uri(source_uri)
-        name = await relay.get_device_name(target)
+        device_name = await relay.get_device_name(target)
         await relay.say_and_wait(target, 'What is your name?')
-        user = await relay.listen(target, 'request1')
-        await relay.say_and_wait(target, f'Hello {user}! from {name}')
-        await relay.end_interaction(target, 'myinteraction')
+        user_provided_name = await relay.listen(target, 'request1')
+        greeting = await relay.get_var('greeting')
+        await relay.say_and_wait(target, f'{greeting} {user_provided_name}! You are currently using {device_name}')
+        await relay.end_interaction(target, 'hello world')
+    if itype == 'ended':
         await relay.terminate()
