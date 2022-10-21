289566131e6c886614a44ce29e3c692eac4f36ac
Ashley Godwin <agodwin@relaypro.com>
2022-10-21 10:46:22 -0400

    Added os.chdir call

--- docs/generate_docs.py
+++ docs/generate_docs.py
@@ -1,8 +1,10 @@
 import inspect
 import os
-from relay import workflow
+import relay.workflow
 
+os.chdir('../relay')
+os.system('ls')
 os.system('pydoc3 -w workflow')
-for name, obj in inspect.getmembers(workflow):
+for name, obj in inspect.getmembers(relay.workflow):
     if inspect.isclass(obj):
         os.system('pydoc3 -w workflow.%s' % name)
